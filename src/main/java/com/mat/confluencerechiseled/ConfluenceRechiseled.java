2a8ef2339416e49d82735a338cc5ec9046ca20b5
MAT Services Administrator <157519566+MAT-services@users.noreply.github.com>
2026-05-16 14:46:19 +0200

    Delete Config.

--- src/main/java/com/mat/confluencerechiseled/ConfluenceRechiseled.java
+++ src/main/java/com/mat/confluencerechiseled/ConfluenceRechiseled.java
@@ -48,7 +48,7 @@ public class ConfluenceRechiseled {
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public ConfluenceRechiseled(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
+        //modEventBus.addListener(this::commonSetup);
 
         // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
@@ -61,10 +61,10 @@ public class ConfluenceRechiseled {
         NeoForge.EVENT_BUS.register(this);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        //modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
+    /*private void commonSetup(FMLCommonSetupEvent event) {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
 
@@ -75,13 +75,13 @@ public class ConfluenceRechiseled {
         LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
 
         Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
-    }
+    }*/
 
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        //LOGGER.info("HELLO from server starting");
     }
 }
